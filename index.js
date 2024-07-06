71ff4f74feddd7b3470e27ffa4f52a1022a48b31
MD:MAZEDUL ISLAM <125912440+mazed2020@users.noreply.github.com>
2024-07-06 20:09:19 +0000

    adding functionallity but game function cant work yet now

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let boxes=document.querySelectorAll(".box");
 let resetBtn=document.querySelector(".reset-btn");
 let winning_puttern=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 let turn=true;
+let count=0;
 //  for (const patturn of winning_puttern) {
 //      let pos1=patturn[0];
 //      let pos2=patturn[1];
@@ -17,11 +18,13 @@ let turn=true;
 /*add eventlisener in all button
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 resetBtn.addEventListener('click',()=>{
+    document.querySelector("#winnershow").innerHTML="";
     boxes.forEach((box)=>{
+        box.disabled=false;
         box.innerText="";
     })
 })
-boxes.forEach((box)=>{
+boxes.forEach((box)=>{   
 box.addEventListener('click',()=>{
      if(turn){
         box.innerText='O';
@@ -31,5 +34,41 @@ box.addEventListener('click',()=>{
         box.innerText='X';
         turn=true;
      }
+       box.disabled=true;
+       count++;
+     let winner=findWinner();
+     if(count===9 && winner){
+        gameDrow();
+
+     }
 })
-})
+});
+function findWinner(){
+    for(let patturn of winning_puttern){
+        let pos1=boxes[patturn[0]].innerHTML;
+        let pos2=boxes[patturn[1]].innerHTML;
+        let pos3=boxes[patturn[2]].innerHTML;
+        if(pos1 !="" && pos2 !="" && pos3 !=""){
+            if(pos1==pos2 && pos2==pos3 && pos3 ==pos1){
+                showWinner(pos1);
+                // console.log(`winner is ${pos1}`);
+                return true;
+            }
+        }
+
+    }
+}
+function gameDrow(){
+    let h2=document.createElement('h2');
+     h2.innerHTML="game is draw play again press reset btn";
+    document.querySelector('#winnershow').appendChild(h2);
+
+}
+function showWinner(value){
+    let h2=document.createElement('h2');
+    h2.innerHTML=`winner is ${value}`
+    h2.style.color="red";
+    // let text=document.createTextNode(`winner is ${value}`);
+    // h2.appendChild(text);
+    document.querySelector('#winnershow').appendChild(h2);
+}
